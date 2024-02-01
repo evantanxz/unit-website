bd54cb574df83c98fb26ab907f122511880146b5
shrall <shrallvierdo@gmail.com>
2024-02-01 08:29:48 +0700

    feat: move mobile hamburger icon to right

---
+++ src/theme/Navbar/MobileSidebar/Toggle/index.js
@@ -0,0 +1,24 @@
+import { translate } from "@docusaurus/Translate";
+import { useNavbarMobileSidebar } from "@docusaurus/theme-common/internal";
+import IconMenu from "@theme/Icon/Menu";
+import React from "react";
+export default function MobileSidebarToggle() {
+  const { toggle, shown } = useNavbarMobileSidebar();
+  return (
+    <button
+      onClick={toggle}
+      aria-label={translate({
+        id: "theme.docs.sidebar.toggleSidebarButtonAriaLabel",
+        message: "Toggle navigation bar",
+        description:
+          "The ARIA label for hamburger menu button of mobile navigation",
+      })}
+      aria-expanded={shown}
+      className="navbar__toggle clean-btn"
+      type="button"
+      style={{ marginRight: 0 }}
+    >
+      <IconMenu />
+    </button>
+  );
+}
