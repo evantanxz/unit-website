f0471a535fc07b2753cd6c264a61c80fb41800f8
Marshall Kurniawan <shrallvierdo@gmail.com>
2024-03-06 10:40:29 +0700

    feat(docs): add boxes on core tokens page

--- src/components/GridBox.jsx
+++ src/components/GridBox.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-function GridBox({ title, link }) {
+function GridBox({ title, link, description }) {
   return (
     <div className="grid-box">
       <p>{title}</p>
-      <a href={link} style={{ wordBreak: "break-all" }}>
-        {link}
-      </a>
+      {description && <p>{description}</p>}
+      {link && (
+        <a href={link} style={{ wordBreak: "break-all" }}>
+          {link}
+        </a>
+      )}
     </div>
   );
 }
